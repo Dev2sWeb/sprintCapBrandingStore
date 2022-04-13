516f2421b1637cbdc950ec93aace792c5aadff87
drajput1 <drajput1@INLEN8520015285.corp.capgemini.com>
2022-04-13 11:37:24 +0530

    First Commit....

---
+++ src/main/java/com/CapBrandingStore/mainCodes/SprintCapBrandingStoreApplication.java
@@ -0,0 +1,13 @@
+package com.CapBrandingStore.mainCodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintCapBrandingStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintCapBrandingStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CapBrandingStore/mainCodes/SprintCapBrandingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CapBrandingStore.mainCodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintCapBrandingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
